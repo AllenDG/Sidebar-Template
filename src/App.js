943d9fe6dff405600827e867cbce03b2851deceb
AllenDG <allenwalterfiestada@gmail.com>
2023-09-01 17:47:15 +0800

    sidebar template using mui components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import ButtonAppbar from "./components/navbar/Navbar"
+import Home from "./components/pages/home/Home"
+import Appointment from "./components/pages/appointment/Appointment"
+import DoctorList from "./components/pages/doctorList/DoctorList"
+import About from "./components/pages/about/About"
+
+import ProfileSettings from "./components/pages/profileSettings/ProfileSettings"
+import Settings from "./components/pages/settings/Settings"
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
 
 function App() {
+  sum(3,5);
+  function sum(a,b) {
+    console.log(a,b)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+
+        <ButtonAppbar></ButtonAppbar>
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/appointment" element={<Appointment />}></Route>
+          <Route path="/doctorList" element={<DoctorList />}></Route>
+          <Route path="/about" element={<About />}> </Route>
+
+          <Route path="/profileSettings" element={<ProfileSettings />}></Route>
+          <Route path="/settings" element={<Settings />}></Route>
+        </Routes>
+      
+      </div>
+    </Router>
   );
 }
 
